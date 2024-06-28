390f30b046b506f4d944d9f44d56470bd15bdd53
zhangtianyu07 <zhangtianyu07@baidu.com>
2024-06-28 18:10:26 +0800

    完善界面

--- ai_note/note.py
+++ ai_note/note.py
@@ -35,10 +35,9 @@ class AINote():
             api_key="Rm5kUVRwQUIzelVaTnExMm1FZ2g6Q2g4U05ON1BTOS1GazJGRm9yTlJQdw=="
         )
 
-        if drop_exists:
-            self.drop_index("work")
-            self.drop_index("schedule")
-            self.drop_index("knowledge")
+        self.init_index("work", drop_exists)
+        self.init_index("schedule", drop_exists)
+        self.init_index("knowledge", drop_exists)
         
         self.embedding_model = openai.AzureOpenAI(
             api_key="c851dabeeea24771a17b0ca07f38379c",
@@ -49,13 +48,21 @@ class AINote():
         self._decision_app_id = "bf5c18b4-8244-4d8f-b290-af6573076e7b" # 决策的应用ID 
         self._analysis_app_id = "a5645664-645b-4d7b-860f-887d851cc3e6" # 分析的应用ID
 
-    def drop_index(self, index_name: str):
-        """删除索引"""
+        self.builder = appbuilder.AppBuilderClient(self._decision_app_id)
+        self.conversation_id = self.builder.create_conversation()
+
+    def init_index(self, index_name: str, drop_exists: bool):
+        """初始化索引"""
         if self.es.indices.exists(index=index_name):
-            response = self.es.indices.delete(index=index_name)
-            print(f"Index {index_name} deleted: {response}")
+            print(f"Index {index_name} already exists.")
+            if drop_exists:
+                response = self.es.indices.delete(index=index_name)
+                print(f"Index {index_name} deleted: {response}")
+                response = self.es.indices.create(index=index_name, body={})
+                print(f"Index {index_name} created: {response}")
         else:
-            print(f"Index {index_name} does not exist.")
+            response = self.es.indices.create(index=index_name, body={})
+            print(f"Index {index_name} created: {response}")
 
     def embedding(self, content: str):
         """嵌入"""
@@ -65,62 +72,31 @@ class AINote():
         )
         return embedding_result.data[0].embedding
 
-    def save(self, index_name: str, work_time: str, work_detail: str):
+    def save(self, user_id: str, index_name: str, work_time: str, work_detail: str):
         """保存笔记"""
         timestamp = to_timestamp(work_time)
         content = f"{work_time}：{work_detail}"
         response = self.es.index(index=index_name, body={
+            "user_id": user_id,
             "timestamp": timestamp,
             "content": content,
             "vector": self.embedding(content)
         })
         print(f"Document added: {response} in {index_name}")
 
-    def query_with_embedding(self, index_name: str, content: str, k: int = 1):
-        """查询笔记"""
-        query_vector = self.embedding(content)
-        resp = self.es.search(index=index_name, knn={
-            "field": "vector",
-            "query_vector": query_vector,
-            "k": k,
-            "num_candidates": 100,
-        })
-        return resp
-
-    def query_with_time_range(self, index_name: str, start_time: str, end_time: str, k: int = 10):
-        """查询笔记"""
-        resp = self.es.search(
-            index=index_name,
-            size=k,  # 限制返回的文档数量为k
-            body={
-                "query": {
-                    "range": {
-                        "timestamp": {
-                            "gte": to_timestamp(start_time),
-                            "lte": to_timestamp(end_time)
-                        }
-                    }
-                }
-            }
-        )
-        return resp
-
-    def query_with_embedding_and_time_range(self, index_name: str, content: str, start_time: str, end_time: str, k: int = 1):
+    def query_with_embedding(self, user_id: str, index_name: str, content: str, k: int = 1):
         """查询笔记"""
         query_vector = self.embedding(content)
         resp = self.es.search(
             index=index_name,
-            size=k,  # 限制返回的文档数量为k
+            size=k,
             body={
                 "query": {
                     "bool": {
                         "must": [
                             {
-                                "range": {
-                                    "timestamp": {
-                                        "gte": to_timestamp(start_time),
-                                        "lte": to_timestamp(end_time)
-                                    }
+                                "term": {
+                                    "user_id": user_id  # 限制user_id字段为指定的user_id
                                 }
                             },
                             {
@@ -141,31 +117,110 @@ class AINote():
         )
         return resp
 
-    def delete_with_time_range(self, index_name: str, start_time: str, end_time: str):
+    def query_with_time_range(self, user_id: str, index_name: str, start_time: str, end_time: str, k: int = 10):
+        """查询笔记"""
+        resp = self.es.search(
+            index=index_name,
+            size=k,  # 限制返回的文档数量为k
+            body={
+                "query": {
+                    "bool": {
+                        "must": [
+                            {
+                                "range": {
+                                    "timestamp": {
+                                        "gte": to_timestamp(start_time),
+                                        "lte": to_timestamp(end_time)
+                                    }
+                                }
+                            },
+                            {
+                                "term": {
+                                    "user_id": user_id  # 限制user_id字段为指定的user_id
+                                }
+                            }
+                        ]
+                    }
+                }
+            }
+        )
+        return resp
+
+    def query_with_embedding_and_time_range(self, user_id: str, index_name: str, content: str, start_time: str, end_time: str, k: int = 1):
+        """查询笔记"""
+        query_vector = self.embedding(content)
+        resp = self.es.search(
+            index=index_name,
+            size=k,  # 限制返回的文档数量为k
+            body={
+                "query": {
+                    "bool": {
+                        "must": [
+                            {
+                                "range": {
+                                    "timestamp": {
+                                        "gte": to_timestamp(start_time),
+                                        "lte": to_timestamp(end_time)
+                                    }
+                                }
+                            },
+                            {
+                                "term": {
+                                    "user_id": user_id  # 限制user_id字段为指定的user_id
+                                }
+                            },
+                            {
+                                "script_score": {
+                                    "query": {"match_all": {}},
+                                    "script": {
+                                        "source": "cosineSimilarity(params.query_vector, 'vector') + 1.0",
+                                        "params": {
+                                            "query_vector": query_vector
+                                        }
+                                    }
+                                }
+                            }
+                        ]
+                    }
+                }
+            }
+        )
+        return resp
+
+    def delete_with_time_range(self, user_id: str, index_name: str, start_time: str, end_time: str):
         """删除笔记"""
         self.es.delete_by_query(
             index=index_name,
             body={
                 "query": {
-                    "range": {
-                        "timestamp": {
-                            "gte": to_timestamp(start_time),
-                            "lte": to_timestamp(end_time)
-                        }
+                    "bool": {
+                        "must": [
+                            {
+                                "range": {
+                                    "timestamp": {
+                                        "gte": to_timestamp(start_time),
+                                        "lte": to_timestamp(end_time)
+                                    }
+                                }
+                            },
+                            {
+                                "term": {
+                                    "user_id": user_id  # 限制user_id字段为指定的user_id
+                                }
+                            }
+                        ]
                     }
                 }
             }
         )
         print(f"Documents in the time range deleted successfully.")
 
-    def call_gpt(self, sapce: str, msg: str, retry = 0) -> str:
+    def call_gpt(self, user_id: str, sapce: str, msg: str) -> str:
         """调用模型"""
-        if retry >= 3:
-            return "对不起，我无法理解您的问题。"
-
-        builder = appbuilder.AppBuilderClient(self._decision_app_id)
-        conversation_id = builder.create_conversation()
-        run_result = builder.run(conversation_id, msg)
+        # builder = appbuilder.AppBuilderClient(self._decision_app_id)
+        # conversation_id = builder.create_conversation()
+        # 使用历史上下文
+        run_result = self.builder.run(self.conversation_id, msg)
         print(run_result.model_dump_json(indent=4))
         client_answer: data_class.AppBuilderClientAnswer = run_result.content
         event: data_class.Event = client_answer.events[0]
@@ -179,7 +234,7 @@ class AINote():
             # 保存
             work_time = arguments_obj["work_time"]
             work_detail = arguments_obj["work_detail"]
-            self.gpt_do_save(sapce, work_time, work_detail)
+            self.gpt_do_save(user_id, sapce, work_time, work_detail)
             return client_answer.answer
         
         if "keyword" in arguments_obj:
@@ -187,48 +242,50 @@ class AINote():
             keyword = arguments_obj["keyword"]
             start_time = arguments_obj["start_time"] if "start_time" in arguments_obj else ""
             end_time = arguments_obj["end_time"] if "end_time" in arguments_obj else ""
-            return self.gpt_do_query(sapce, msg, keyword, start_time, end_time)
+            return self.gpt_do_query(user_id, sapce, msg, keyword, start_time, end_time)
 
         if "delete_keyword" in arguments_obj:
             # 删除
             start_time = arguments_obj["start_time"] if "start_time" in arguments_obj else ""
             end_time = arguments_obj["end_time"] if "end_time" in arguments_obj else ""
-            delete_resp = self.gpt_do_delete(sapce, start_time, end_time)
+            delete_resp = self.gpt_do_delete(user_id, sapce, start_time, end_time)
             if delete_resp == "":
                 delete_resp = client_answer.answer
             return delete_resp
 
-        # 返回格式不正确，再次尝试
-        return self.call_gpt(sapce, msg, retry + 1)
+        return client_answer.answer
 
-    def gpt_do_save(self, space: str, work_time: str, work_detail: str):
+    def gpt_do_save(self, user_id: str, space: str, work_time: str, work_detail: str):
         """保存"""
-        self.save(space, work_time, work_detail)
+        self.save(user_id, space, work_time, work_detail)
     
-    def gpt_do_query(self, space: str, msg: str, keyword: str, start_time: str, end_time: str) -> str:
+    def gpt_do_query(self, user_id: str, space: str, msg: str, keyword: str, start_time: str, end_time: str) -> str:
         """查询"""
+        content = keyword if keyword != "" else msg
         if start_time != "" and end_time != "":
-            query_result = self.query_with_time_range(space, start_time, end_time)
+            if content != "":
+                query_result = self.query_with_embedding_and_time_range(user_id, space, content, start_time, end_time, 10)
+            else:
+                query_result = self.query_with_time_range(user_id, space, start_time, end_time, 10)
         else:
-            content = keyword if keyword != "" else msg
-            query_result = self.query_with_embedding(space, content, 5)
+            query_result = self.query_with_embedding(user_id, space, content, 10)
         
         records = []
         for hit in query_result["hits"]["hits"]:
             records.append(hit["_source"]["content"])
 
         if len(records) == 0:
-            return "对不起，没有找到相关的记录，请重新确认时间。"
+            return "对不起，没有找到相关的记录，请提供更明确的信息。"
 
         records_to_analysis = "\n".join(records)
         request_message = f"【待分析的记录】\n{records_to_analysis}\n【问题或要求】\n{msg}"
         print(request_message)
         return self._request_analysis(request_message)
     
-    def gpt_do_delete(self, space: str, start_time: str, end_time: str) -> str:
+    def gpt_do_delete(self, user_id: str, space: str, start_time: str, end_time: str) -> str:
         """删除"""
         if start_time != "" and end_time != "":
-            self.delete_with_time_range(space, start_time, end_time)
+            self.delete_with_time_range(user_id, space, start_time, end_time)
             return ""
         return "对不起，删除记录需要提供确切的关键字和时间范围。"
 
--- run_web.py
+++ run_web.py
@@ -1,27 +1,53 @@
 import os
+import uuid
 import gradio as gr
 from ai_note.note import AINote
-import appbuilder
 
 note = AINote(True)
 
-def on_submit_knowledge(user_message, chatbot_messages):
+def on_submit_knowledge(user_id, user_message, chatbot_messages):
     """知识点空间"""
-    result = note.call_gpt("knowledge", user_message)
+    result = note.call_gpt(user_id, "knowledge", user_message)
     chatbot_messages.append([user_message, result])
     return "", chatbot_messages
 
-def on_submit_schedule(user_message, chatbot_messages):
+def on_submit_schedule(user_id, user_message, chatbot_messages):
     """日程空间"""
-    result = note.call_gpt("schedule", user_message)
+    result = note.call_gpt(user_id, "schedule", user_message)
     chatbot_messages.append([user_message, result])
     return "", chatbot_messages
 
-def on_submit_work(user_message, chatbot_messages):
+def on_submit_work(user_id, user_message, chatbot_messages):
     """工作空间"""
-    result = note.call_gpt("work", user_message)
+    result = note.call_gpt(user_id, "work", user_message)
     chatbot_messages.append([user_message, result])
     return "", chatbot_messages
+
+def on_load(user_id):
+    """加载"""
+    if user_id == "":
+        user_id = uuid.uuid4().hex
+    return user_id
+
+
+
+demo_introduction = """<h2>操作指南</h2>
+<h3><产品简介></h3>
+<p><strong>智能云笔记</strong> 为用户提供了强大的笔记管理功能，包括 <strong>存储</strong>、<strong>查询</strong>、<strong>删除</strong> 等操作。</p>
+<h3><用户ID></h3>
+<p>用户的唯一标识，每次刷新会重新生成，也可以用户输入，请妥善保管，切勿外传</p>
+<h3><交互形式></h3>
+<p>对话</p>
+<h3><自定义空间划分></h3>
+<p>可以划分不同的空间来分类和管理您的笔记，不同空间下存储的信息是完全隔离的，当前Demo示意如下：</p>
+<ul>
+  <strong>【工作】</strong>：专注于工作相关的内容，帮助您高效地组织和管理工作笔记。
+  <br>
+  <strong>【日程】</strong>：记录您的日程安排，让您不再错过任何重要的会议和活动。
+  <br>
+  <strong>【知识点】</strong>：存储和管理学习和研究中的重要知识点，让学习更加系统化。
+</ul>
+"""
 
 # -------------------------------------------------------------------
 # | Web UI                                                          |
@@ -30,92 +56,80 @@ def on_submit_work(user_message, chatbot_messages):
 PageTitle = "智能云笔记"
 with gr.Blocks(title=PageTitle) as app:
     gr.Markdown(f"<h1 style='text-align:center;'>{PageTitle}</h1>")
-
-    with gr.Tab("工作"):
-        with gr.Column():
-            chatbot_work = gr.Chatbot(
-                value=[],
-                height=520,
-                bubble_full_width=False,
-                avatar_images=(
-                    (os.path.join(os.path.dirname(__file__), "./assets/human.png")),
-                    (os.path.join(os.path.dirname(__file__), "./assets/autogen.png")),
-                ),
-                show_copy_button=True,
+    with gr.Row():
+        with gr.Column(scale=1):
+            textbox_user_id = gr.Textbox(value="", visible=True, label="用户ID")
+            textarea_introduction = gr.Markdown(
+                label="Demo功能指南", value=demo_introduction
             )
+        with gr.Column(scale=3):
+            with gr.Tab("工作"):
+                with gr.Column():
+                    chatbot_work = gr.Chatbot(
+                        value=[],
+                        height=520,
+                        bubble_full_width=False,
+                        avatar_images=(
+                            (os.path.join(os.path.dirname(__file__), "./assets/human.png")),
+                            (os.path.join(os.path.dirname(__file__), "./assets/autogen.png")),
+                        ),
+                        show_copy_button=True,
+                    )
 
-            textbox_user_msg_work = gr.Textbox(label="User Input", interactive=True)
-            button_submit_work = gr.Button(value="提交", variant="primary")
+                    textbox_user_msg_work = gr.Textbox(label="用户输入", interactive=True)
+                    button_submit_work = gr.Button(value="提交工作", variant="primary")
 
-    with gr.Tab("日程"):
-        with gr.Column():
-            chatbot_schedule = gr.Chatbot(
-                value=[],
-                height=520,
-                bubble_full_width=False,
-                avatar_images=(
-                    (os.path.join(os.path.dirname(__file__), "./assets/human.png")),
-                    (os.path.join(os.path.dirname(__file__), "./assets/autogen.png")),
-                ),
-                show_copy_button=True,
-            )
-            textbox_user_msg_schedule = gr.Textbox(label="User Input", interactive=True)
-            button_submit_schedule = gr.Button(value="提交", variant="primary")
+            with gr.Tab("日程"):
+                with gr.Column():
+                    chatbot_schedule = gr.Chatbot(
+                        value=[],
+                        height=520,
+                        bubble_full_width=False,
+                        avatar_images=(
+                            (os.path.join(os.path.dirname(__file__), "./assets/human.png")),
+                            (os.path.join(os.path.dirname(__file__), "./assets/autogen.png")),
+                        ),
+                        show_copy_button=True,
+                    )
+                    textbox_user_msg_schedule = gr.Textbox(label="用户输入", interactive=True)
+                    button_submit_schedule = gr.Button(value="提交日程", variant="primary")
 
-    with gr.Tab("知识点"):
-        with gr.Column():
-            chatbot_knowledge = gr.Chatbot(
-                value=[],
-                height=520,
-                bubble_full_width=False,
-                avatar_images=(
-                    (os.path.join(os.path.dirname(__file__), "./assets/human.png")),
-                    (os.path.join(os.path.dirname(__file__), "./assets/autogen.png")),
-                ),
-                show_copy_button=True,
-            )
-            textbox_user_msg_knowledge = gr.Textbox(label="User Input", interactive=True)
-            button_submit_knowledge = gr.Button(value="提交", variant="primary")
+            with gr.Tab("知识点"):
+                with gr.Column():
+                    chatbot_knowledge = gr.Chatbot(
+                        value=[],
+                        height=520,
+                        bubble_full_width=False,
+                        avatar_images=(
+                            (os.path.join(os.path.dirname(__file__), "./assets/human.png")),
+                            (os.path.join(os.path.dirname(__file__), "./assets/autogen.png")),
+                        ),
+                        show_copy_button=True,
+                    )
+                    textbox_user_msg_knowledge = gr.Textbox(label="用户输入", interactive=True)
+                    button_submit_knowledge = gr.Button(value="提交知识", variant="primary")
 
 # -------------------------------------------------------------------
 # | Events                                                          |
 # -------------------------------------------------------------------
 
-    button_submit_work.click(
-        on_submit_work,
-        inputs=[
-            textbox_user_msg_work,
-            chatbot_work
-        ],
-        outputs=[
-            textbox_user_msg_work,
-            chatbot_work
-        ]
-    )
+    work_inputs = [textbox_user_id, textbox_user_msg_work, chatbot_work]
+    work_outputs = [textbox_user_msg_work, chatbot_work]
+    textbox_user_msg_work.submit(on_submit_work, inputs=work_inputs, outputs=work_outputs)
+    button_submit_work.click(on_submit_work, inputs=work_inputs, outputs=work_outputs)
 
-    button_submit_schedule.click(
-        on_submit_schedule,
-        inputs=[
-            textbox_user_msg_schedule,
-            chatbot_schedule
-        ],
-        outputs=[
-            textbox_user_msg_schedule,
-            chatbot_schedule
-        ]
-    )
+    schedule_inputs = [textbox_user_id, textbox_user_msg_schedule, chatbot_schedule]
+    schedule_outputs = [textbox_user_msg_schedule, chatbot_schedule]
+    textbox_user_msg_schedule.submit(on_submit_schedule, inputs=schedule_inputs, outputs=schedule_outputs)
+    button_submit_schedule.click(on_submit_schedule, inputs=schedule_inputs, outputs=schedule_outputs)
 
-    button_submit_knowledge.click(
-        on_submit_knowledge,
-        inputs=[
-            textbox_user_msg_knowledge,
-            chatbot_knowledge
-        ],
-        outputs=[
-            textbox_user_msg_knowledge,
-            chatbot_knowledge
-        ]
-    )
+    knowledge_inputs = [textbox_user_id, textbox_user_msg_knowledge, chatbot_knowledge]
+    knowledge_outputs = [textbox_user_msg_knowledge, chatbot_knowledge]
+    textbox_user_msg_knowledge.submit(on_submit_knowledge, inputs=knowledge_inputs, outputs=knowledge_outputs)
+    button_submit_knowledge.click(on_submit_knowledge, inputs=knowledge_inputs, outputs=knowledge_outputs)
+
+    app.load(on_load, inputs=[textbox_user_id], outputs=[textbox_user_id])
+    
 
 def launch(server_port=8895):
     """启动web服务"""
